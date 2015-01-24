cf9567e4de3693b43d932837c0e1bec979182c46
Edward Anderson <nilbus@nilbus.com>
2015-01-24 13:01:18 -0500

    Remove guard exit code check; even on failure, it exits successfully

--- spec/acceptance/guard-jruby-rspec_spec.rb
+++ spec/acceptance/guard-jruby-rspec_spec.rb
@@ -7,10 +7,6 @@ describe 'The jruby-rspec guard' do
 
   let(:run_guard_then_exit) { 'true | bundle exec guard' }
 
-  it 'exits with a successful exit code' do
-    expect(system("#{run_guard_then_exit} >/dev/null 2>&1")).to be_truthy
-  end
-
   it 'runs the passing rspec test suite' do
     output = `#{run_guard_then_exit} 2>&1`
     expect(output).to include '0 failures'
